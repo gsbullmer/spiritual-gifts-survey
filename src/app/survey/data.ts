4741a392f99c5063a6273a8c8e3be37c194bc82b
Greg Bullmer <gsbullmer@gmail.com>
2023-07-09 03:02:12 -0500

    add initial data file

---
+++ src/app/survey/data.ts
@@ -0,0 +1,313 @@
+export const LikertScale = [
+  { value: -2, label: "Rarely true \n of me" },
+  { value: -1, label: "Infrequently \n true of me" },
+  { value: 0, label: "Occasionally \n true of me" },
+  { value: 1, label: "Frequently \n true of me" },
+  { value: 2, label: "Very true of me, \n consistently" },
+];
+
+export const Gifts = [
+  {
+    id: "g1",
+    name: "Adminstration",
+    slug: "adminstration",
+  },
+  {
+    id: "g2",
+    name: "Artistry/Creativity",
+    slug: "artistry-creativity",
+  },
+  {
+    id: "g3",
+    name: "Builder",
+    slug: "builder",
+  },
+  {
+    id: "g4",
+    name: "Compassion",
+    slug: "compassion",
+  },
+  {
+    id: "g5",
+    name: "Encouragement",
+    slug: "encouragement",
+  },
+  {
+    id: "g6",
+    name: "Evangelism",
+    slug: "evangelism",
+  },
+  {
+    id: "g7",
+    name: "Faith",
+    slug: "faith",
+  },
+  {
+    id: "g8",
+    name: "Generosity",
+    slug: "generosity",
+  },
+  {
+    id: "g9",
+    name: "Helper/Service",
+    slug: "helper-service",
+  },
+  {
+    id: "g10",
+    name: "Hospitality",
+    slug: "hospitality",
+  },
+  {
+    id: "g11",
+    name: "Leadership",
+    slug: "leadership",
+  },
+  {
+    id: "g12",
+    name: "Shepherding",
+    slug: "shepherding",
+  },
+  {
+    id: "g13",
+    name: "Teaching",
+    slug: "teaching",
+  },
+  {
+    id: "g14",
+    name: "Music/Singing",
+    slug: "music-singing",
+  },
+  {
+    id: "g15",
+    name: "Writing",
+    slug: "writing",
+  },
+];
+
+export const Questions = [
+  {
+    id: "q1",
+    text: "When presented a goal, I immediately think of steps that need to be taken in order to achieve the desired results.",
+    giftId: "g1",
+  },
+  {
+    id: "q2",
+    text: "I express myself through artistic means.",
+    giftId: "g2",
+  },
+  {
+    id: "q3",
+    text: "I enjoy working with my hands in a trade or skill that requires considerable experience to perfect.",
+    giftId: "g3",
+  },
+  {
+    id: "q4",
+    text: "When I see a person in need, I am moved to assist them.",
+    giftId: "g4",
+  },
+  {
+    id: "q5",
+    text: "I am moved by those who through conflict or sorrow are wavering in faith.",
+    giftId: "g5",
+  },
+  {
+    id: "q6",
+    text: "I am able to convey the Gospel message to others in ways that they are able to easily understand.",
+    giftId: "g6",
+  },
+  {
+    id: "q7",
+    text: "I am certain of the spirit's presence in my life and the lives of others.",
+    giftId: "g7",
+  },
+  {
+    id: "q8",
+    text: "I am blessed by God each day and gladly respond to these blessings by giving liberally of my time and money.",
+    giftId: "g8",
+  },
+  {
+    id: "q9",
+    text: "I like working behind the scenes to ensure projects are successful.",
+    giftId: "g9",
+  },
+  {
+    id: "q10",
+    text: "I enjoy meeting new people and becoming acquainted with them.",
+    giftId: "g10",
+  },
+  {
+    id: "q11",
+    text: "I am a take charge person. When others follow my direction, the goal or task will be completed.",
+    giftId: "g11",
+  },
+  {
+    id: "q12",
+    text: "I am motivated to provide spiritual leadership to those who are on a faith journey.",
+    giftId: "g12",
+  },
+  {
+    id: "q13",
+    text: "My great joy is to communicate biblical truth in such a way that it becomes real and understood by others.",
+    giftId: "g13",
+  },
+  {
+    id: "q14",
+    text: "I love to sing and/or play an instrument and enjoy inspiring others through music.",
+    giftId: "g14",
+  },
+  {
+    id: "q15",
+    text: "I can take a thought or idea and put it into a clear and inspiring written form.",
+    giftId: "g15",
+  },
+  {
+    id: "q16",
+    text: "I enjoy organizing thoughts, ideas, hopes and dreams into a specific plan of action.",
+    giftId: "g1",
+  },
+  {
+    id: "q17",
+    text: "I can translate into artistic form what I first see in my imagination.",
+    giftId: "g2",
+  },
+  {
+    id: "q18",
+    text: "I am good at building, repairing, or restoring things and find satisfaction in doing so.",
+    giftId: "g3",
+  },
+  {
+    id: "q19",
+    text: "When I see hurting in the world (natural disasters, homeless, starvation, etc.) I feel an urgency to help out in any way that I can.",
+    giftId: "g4",
+  },
+  {
+    id: "q20",
+    text: "When I know someone is facing a crisis, I feel compelled to provide support and care.",
+    giftId: "g5",
+  },
+  {
+    id: "q21",
+    text: "I enjoy being with people who have questions about faith and religion and like having the opportunity to share my faith story.",
+    giftId: "g6",
+  },
+  {
+    id: "q22",
+    text: "My trust in the presence of Christ when I encounter times of personal crisis is a source of strength for others.",
+    giftId: "g7",
+  },
+  {
+    id: "q23",
+    text: "I manage my time and money so that I am able to give much of it to the work of the church or other organizations.",
+    giftId: "g8",
+  },
+  {
+    id: "q24",
+    text: "People tell me that without my willingness to do the unnoticed jobs, their work would be more difficult.",
+    giftId: "g9",
+  },
+  {
+    id: "q25",
+    text: "I enjoy hosting people in my home for social gatherings and special events.",
+    giftId: "g10",
+  },
+  {
+    id: "q26",
+    text: "When I am in a group, others will often look to me for direction.",
+    giftId: "g11",
+  },
+  {
+    id: "q27",
+    text: "I enjoy providing spiritual guidance to an individual or group of Christians.",
+    giftId: "g12",
+  },
+  {
+    id: "q28",
+    text: "I want to express my faith by assisting others to discover the truths contained in the Bible.",
+    giftId: "g13",
+  },
+  {
+    id: "q29",
+    text: "I find joy and express myself by playing a musical instrument or singing my favorite songs.",
+    giftId: "g14",
+  },
+  {
+    id: "q30",
+    text: "I often feel moved to write about my thoughts and feelings so others may benefit from them.",
+    giftId: "g15",
+  },
+  {
+    id: "q31",
+    text: "I have been successful in organizing, directing, and motivating people to achieve a goal.",
+    giftId: "g1",
+  },
+  {
+    id: "q32",
+    text: "My artistic work has given spiritual strength and inspiration to others.",
+    giftId: "g2",
+  },
+  {
+    id: "q33",
+    text: "My knowledge of building, maintenance, or repair has been a special value to the church and others.",
+    giftId: "g3",
+  },
+  {
+    id: "q34",
+    text: "People have been surprised by how at ease I am while working with those who are suffering in mind, body, or spirit.",
+    giftId: "g4",
+  },
+  {
+    id: "q35",
+    text: "Those who are struggling with life questions have come to me for guidance and help.",
+    giftId: "g5",
+  },
+  {
+    id: "q36",
+    text: "I often invite others to church activities.",
+    giftId: "g6",
+  },
+  {
+    id: "q37",
+    text: "I can see great things happening in my congregation and am not derailed by the pessimism of others.",
+    giftId: "g7",
+  },
+  {
+    id: "q38",
+    text: "When I receive money unexpectedly, one of my first thoughts is to share this gift through the church.",
+    giftId: "g8",
+  },
+  {
+    id: "q39",
+    text: "When I turn out the lights, take tables down, work in the kitchen or put chairs away, I feel that I have served the Lord.",
+    giftId: "g9",
+  },
+  {
+    id: "q40",
+    text: "I enjoy welcoming guests and helping them to feel at ease.",
+    giftId: "g10",
+  },
+  {
+    id: "q41",
+    text: "People have said they like to work with me because the task will be successfully completed.",
+    giftId: "g11",
+  },
+  {
+    id: "q42",
+    text: "People have come to me for spiritual guidance, and it has developed into a long-term relationship.",
+    giftId: "g12",
+  },
+  {
+    id: "q43",
+    text: "People have told me that I can take the most difficult idea or concept and make it understandable.",
+    giftId: "g13",
+  },
+  {
+    id: "q44",
+    text: "Others have told me they were moved by my playing a musical instrument or singing.",
+    giftId: "g14",
+  },
+  {
+    id: "q45",
+    text: "My written work has been helpful to others in understanding life's truths.",
+    giftId: "g15",
+  },
+];
