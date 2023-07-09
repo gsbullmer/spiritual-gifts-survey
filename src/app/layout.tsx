ae30c2fc2ae22d7ca26f592f0ef0b3bd61f18f00
Greg Bullmer <gsbullmer@gmail.com>
2023-07-08 21:45:03 -0500

    Create main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`container mx-auto p-8 ${inter.className}`}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,85 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <>
+      <div className="flex gap-4">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          src="https://via.placeholder.com/400x250"
+          width="400"
+          height="250"
+          alt="image"
+          className="aspect-square"></Image>
+        <Image
+          src="https://via.placeholder.com/400x250"
+          width="400"
+          height="250"
+          alt="image"
+          className="aspect-video"></Image>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="my-8">
+        <h1 className="text-3xl font-bold text-blue-900">
+          <span className="uppercase">Step 1</span> – Discover Your Gifts
+        </h1>
+        <p>
+          Every child of God is filled with the power of the Holy Spirit, specially gifted to play a unique and valued
+          role in the body of Christ. No one is without gift or purpose. Spiritual gifts are not our talents or skills.
+          They are the grace of God at work within us, empowering us to match our deep passions with the world’s deep
+          need.
+        </p>
+        <Link href="/survey" className="block w-fit bg-green-700 text-white text-lg font-bold uppercase py-4 px-8 rounded-md my-4 mx-auto">Take the Spiritual Gifts Assessment</Link>
       </div>
-    </main>
-  )
-}
+      <div className="my-8">
+        <h1 className="text-3xl font-bold text-blue-900">
+          <span className="uppercase">Step 2</span> – Explore Your Gifts
+        </h1>
+        <p>
+          Learn more about your spiritual gifts, what each one means, where read about it in Scripture, and ideas on how
+          to live it out.
+        </p>
+        <ul className="grid grid-cols-5 gap-x-16 w-fit mx-auto">
+          <Link href="/gifts/administration"><li className="w-fit">Administration</li></Link>
+          <Link href="/gifts/artistry-creativity"><li className="w-fit">Artistry/Creativity</li></Link>
+          <Link href="/gifts/builder"><li className="w-fit">Builder</li></Link>
+          <Link href="/gifts/compassion"><li className="w-fit">Compassion</li></Link>
+          <Link href="/gifts/encouragement"><li className="w-fit">Encouragement</li></Link>
+          <Link href="/gifts/evangelism"><li className="w-fit">Evangelism</li></Link>
+          <Link href="/gifts/faith"><li className="w-fit">Faith</li></Link>
+          <Link href="/gifts/generosity"><li className="w-fit">Generosity</li></Link>
+          <Link href="/gifts/helper-service"><li className="w-fit">Helper/Service</li></Link>
+          <Link href="/gifts/hospitality"><li className="w-fit">Hospitality</li></Link>
+          <Link href="/gifts/leadership"><li className="w-fit">Leadership</li></Link>
+          <Link href="/gifts/music-singing"><li className="w-fit">Music/Singing</li></Link>
+          <Link href="/gifts/shepherding"><li className="w-fit">Shepherding</li></Link>
+          <Link href="/gifts/teaching"><li className="w-fit">Teaching</li></Link>
+          <Link href="/gifts/writing"><li className="w-fit">Writing</li></Link>
+        </ul>
+      </div>
+      <div className="my-8">
+        <h1 className="text-3xl font-bold text-blue-900">
+          <span className="uppercase">Step 3</span> – Share Your Gifts
+        </h1>
+        <p>
+          All gifts are significant, and all come from the same Spirit. They are all meant to point people to God, and
+          they are to be used for the good of all. Specific ways to share your gifts within different ministries can be
+          explored in more detail. Contact the ministry leader to get plugged in or complete the Service Interest Form
+          and we will follow up with you. We look forward to serving alongside you!
+        </p>
+        <Link href="#" className="block w-fit bg-blue-900 text-white text-lg font-bold uppercase py-4 px-8 rounded-md my-4 mx-auto">Service Interest Form</Link>
+        <ul className="grid grid-cols-4 gap-x-16 w-fit mx-auto">
+          <Link href="#"><li className="w-fit">Youth Ministry</li></Link>
+          <Link href="#"><li className="w-fit">Children Ministry</li></Link>
+          <Link href="#"><li className="w-fit">Care Ministries</li></Link>
+          <Link href="#"><li className="w-fit">Mission Ministry</li></Link>
+          <Link href="#"><li className="w-fit">Sanctuary</li></Link>
+          <Link href="#"><li className="w-fit">Wellspring</li></Link>
+          <Link href="#"><li className="w-fit">Welcoming</li></Link>
+          <Link href="#"><li className="w-fit">Other</li></Link>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Home;
